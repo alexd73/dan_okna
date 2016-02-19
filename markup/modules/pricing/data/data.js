1baa841ed01baef72474d53e6e2cb737910f0ba2
alexd <alexd73@gmail.com>
2016-02-19 18:10:17 +0300

    final 20160219

--- markup/modules/pricing/data/data.js
+++ markup/modules/pricing/data/data.js
@@ -2,16 +2,16 @@ pricing: {
     items: [
         {
             title: 'Двустворчатое окно',
-            price: 'от 4 730 руб.',
+            price: 'от 6 390 руб.',
             size: '1 300 x 1 400 мм',
-            profile_width: '60 мм',
+            profile_width: '58-60 мм.',
             ropr: '0,64м2°C/Вт',
             classes: 'wow fadeInLeft'
         },
         {
             title: 'Трехстворчатое окно',
             popular: true,
-            price: 'от 4 730 руб.',
+            price: 'от 8 990 руб.',
             size: '1 300 x 1 400 мм',
             profile_width: '60 мм',
             ropr: '0,64м2°C/Вт',
@@ -19,7 +19,7 @@ pricing: {
         },
         {
             title: 'Балконный блок',
-            price: 'от 4 730 руб.',
+            price: 'от 10 930 руб.',
             size: '1 300 x 1 400 мм',
             profile_width: '60 мм',
             ropr: '0,64м2°C/Вт',
--- markup/static/misc/sendmail.php
+++ markup/static/misc/sendmail.php
@@ -1,15 +1,15 @@
 <?php
 // Email Submit
 // Note: filter_var() requires PHP >= 5.2.0
-if ( isset($_POST['phone']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+// if ( isset($_POST['phone']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
  
-  // detect & prevent header injections
-  $test = "/(content-type|bcc:|cc:|to:)/i";
-  foreach ( $_POST as $key => $val ) {
-    if ( preg_match( $test, $val ) ) {
-      exit;
-    }
-  }
+//   // detect & prevent header injections
+//   $test = "/(content-type|bcc:|cc:|to:)/i";
+//   foreach ( $_POST as $key => $val ) {
+//     if ( preg_match( $test, $val ) ) {
+//       exit;
+//     }
+//   }
 
 $headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
     'Reply-To: ' . $_POST["email"] . "\r\n" .
@@ -20,5 +20,5 @@ $headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
  
   //      ^
   //  Replace with your email 
-}
+//}
 ?>
